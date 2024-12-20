a05401f3ef6816d5797215569446371ff8fb21eb
danielpsrb <danielpsrb01@gmail.com>
2024-12-20 22:42:52 +0800

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -21,7 +23,33 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'clinic_id',
+        'specialist_id',
+        'roles',
+        'google_id',
+        'ktp_number',
+        'birth_date',
+        'gender',
+        'phone_number',
+        'address',
+        'certification',
+        'telemedicine_fee',
+        'chat_fee',
+        'start_time',
+        'end_time',
+        'photo',
+        'status',
     ];
+
+    public function clinic(): BelongsTo
+    {
+        return $this->belongsTo(Clinic::class);
+    }
+
+    public function specialization(): BelongsTo
+    {
+        return $this->belongsTo(Specialist::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
